9f75050325e4f25e3ffdc5d7ef7ef6f4a051be05
Chantel Cooke <chantelmcooke@gmail.com>
2021-12-16 17:39:28 -0700

    finished project

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+// import {Component} from 'react';
+
+// const styles = {
+//   link: {
+//     cursor: 'pointer',
+//     textDecoration: 'underline',
+//     color: 'blue',
+//     marginLeft: '5px',
+//     marginRight: '5px',
+//   }
+// }
+
+// const filterLink = (current, title, setFilter) => {
+//   if (current === title)
+//     return <span>{item}</span>
+//   else
+//     return <span style={styles.link} onClick={() => setFilter (item) }>{item}</span>  
+// }
+
+// const Footer = ({ filter, setFilter }) => (
+//   <div>
+//     { ['All', 'Active', 'Complete'].map( f => filterLink(filter, f, setFilter) ) }
+//   </div>
+// )
+
+// export default Footer;
